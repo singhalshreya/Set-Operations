092b902d677f4143402314ce59768112b1611b9f
Shreya Singhal <106904528+singhalshreya@users.noreply.github.com>
2022-06-22 13:46:37 +0530

    Created setoperations.py
    Different set operations performed on two defined sets.

---
+++ setoperations.py
@@ -0,0 +1,13 @@
+set1 = {0,2,4,6,8}
+set2 = {1,2,3,4,5}
+# union of set1 and set2
+a= set1.union(set2)
+print(a)
+# intersection of set1 and set2
+b= set1.intersection(set2)
+print(b)
+# difference of set1 and set2
+c = set1.difference(set2)
+print(c)
+# symmetric difference of set1 and  set2
+print(set1.symmetric_difference(set2))
